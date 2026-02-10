52fcc677e05d7d72e90214ba8d354c5233f7043f
becasbalancan <becasbalancan@gmail.com>
2026-02-09 22:11:42 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,10 @@ document.addEventListener("DOMContentLoaded", () => {
             slides[currentSlide].classList.remove("active");
             currentSlide = (currentSlide + 1) % slides.length;
             slides[currentSlide].classList.add("active");
-        }, 5000);
+        }, 4000);
     }
 });
+
 
 
 
