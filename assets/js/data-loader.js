83f4f653e0fc6c54951a6cd604eb81c278829908
becasbalancan <becasbalancan@gmail.com>
2026-02-09 20:25:19 -0600

    Add files via upload

---
+++ assets/js/data-loader.js
@@ -0,0 +1,37 @@
+function cargarAtractivos() {
+    fetch("data/atractivos.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo cargar el archivo JSON");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const contenedor = document.getElementById("lugares-container");
+
+            data.forEach(lugar => {
+
+                const link = document.createElement("a");
+                link.href = lugar.url;
+                link.classList.add("lugar-link");
+
+                const card = document.createElement("div");
+                card.classList.add("card-lugar");
+
+                const imgSrc = lugar.imagen || "assets/img/ui/placeholder.jpg";
+
+                card.innerHTML = `
+                    <img src="${imgSrc}" alt="${lugar.nombre}"
+                         onerror="this.src='assets/img/ui/placeholder.jpg'">
+                    <h4>${lugar.nombre}</h4>
+                    <p>${lugar.categoria}</p>
+                `;
+
+                link.appendChild(card);        // ✅ ENVUELVE LA CARD
+                contenedor.appendChild(link); // ✅ AGREGA EL LINK
+            });
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+function cargarComponente(id, ruta) {
+    fetch(ruta)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById(id).innerHTML = html;
+        });
+}
+
+const basePath = window.location.pathname.includes("/pages/")
+    ? "../components/"
+    : "components/";
+
+document.addEventListener("DOMContentLoaded", () => {
+    cargarComponente("header", basePath + "header.html");
+    cargarComponente("sidebar", basePath + "sidebar.html");
+    cargarComponente("footer", basePath + "footer.html");
+    cargarAtractivos();
+});
+
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+setInterval(() => {
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add("active");
+}, 5000); // cambia cada 5 segundos
